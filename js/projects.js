30c68899337fdeb229b432237bcdcbfe2be966e8
AnadoluSaykodeli-Dot <emirozdemir2255@gmail.com>
2026-09-01 14:41:47 +0300

    Proje js

---
+++ js/projects.js
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    id: "dinorun3310",
+
+    title: "Dino Run 3310",
+
+    type: "game",
+    format: "45",
+
+    genre: "arcade",
+    year: 2026,
+
+    cover: "images/dinorun3310.png",
+
+    playable: true,
+    url: "games/dinorun3310/"
+  }
+];
